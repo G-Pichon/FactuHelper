8d200d373b640e68b3c57cb382e73169ba9a4ee9
Guillaume PICHON <Guillaume.PICHON-ext@pernod-ricard.com>
2024-05-25 10:39:51 +0200

    add base code

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import streamlit as st
 st.text_input("TJM numero 1", key="TJ_1",value=650)
 st.text_input("TJM numero 2", key="TJ_2",value=750)
 st.text_input("TJM numero 3", key="TJ_3",value=800)
-#st.text_input("Somme totale à moduler", key="total_sum",value=1500)
 
 total_sum = st.slider("Somme totale à moduler", min_value=0, max_value=100000,step=25)
 
@@ -28,29 +27,3 @@ df_solutions = pd.DataFrame(mesh_array,columns=[f"TJ1 - {TJ_1} €",f"TJ2 - {TJ_
 df_solutions["Total"]=df_solutions.iloc[:, 0]*TJ_1 + df_solutions.iloc[:, 1]*TJ_2 + df_solutions.iloc[:, 2]*TJ_3
 
 st.dataframe(df_solutions[df_solutions["Total"]==total_sum])
-
-
-
-# # Intput
-# TJ_1 = 675
-# TJ_2 = 1000
-# TJ_3 = 900
-# total_sum = 4200
-
-#total_sum = int(st.session_state.total_sum)
-
-# df_solutions[df_solutions["NUM_Solution"]==total_sum].sort_values(by = ["NUM_Range_1","NUM_Range_2","NUM_Range_3"]).iloc[0]
-
-
-# raw_order_one = df_solutions[df_solutions["NUM_Solution"]==total_sum].sort_values(by = ["NUM_Range_1","NUM_Range_2","NUM_Range_3"]).iloc[0]
-# raw_order_two = df_solutions[df_solutions["NUM_Solution"]==total_sum].sort_values(by = ["NUM_Range_2","NUM_Range_1","NUM_Range_3"]).iloc[0]
-# raw_order_three = df_solutions[df_solutions["NUM_Solution"]==total_sum].sort_values(by = ["NUM_Range_3","NUM_Range_2","NUM_Range_1"]).iloc[0]
-
-
-# st.write(f"Solution 1 : {TJ_1}x{raw_order_one.NUM_Range_1} + {TJ_2}x{raw_order_one.NUM_Range_2} + {TJ_3}x{raw_order_one.NUM_Range_3} = {total_sum}")
-# st.write(f"Solution 2 : {TJ_1}x{raw_order_two.NUM_Range_1} + {TJ_2}x{raw_order_two.NUM_Range_2} + {TJ_3}x{raw_order_two.NUM_Range_3} = {total_sum}")
-# st.write(f"Solution 2 : {TJ_1}x{raw_order_three.NUM_Range_1} + {TJ_2}x{raw_order_three.NUM_Range_2} + {TJ_3}x{raw_order_three.NUM_Range_3} = {total_sum}")
-
-# st.write(df_solutions[df_solutions["NUM_Solution"]==total_sum].iloc[0])
-
-
