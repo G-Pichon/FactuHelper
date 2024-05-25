61d4481e703c8b25056158a05a795780542e978c
G-Pichon <138606513+G-Pichon@users.noreply.github.com>
2024-05-25 14:24:42 +0200

    Feat/test release (#2)
    * add pre-commit config
    
    * clean code with pre-commit rules
    
    * layout updates
    
    * linting updates
    
    ---------
    
    Co-authored-by: Guillaume PICHON <Guillaume.PICHON-ext@pernod-ricard.com>

--- app.py
+++ app.py
@@ -1,29 +1,54 @@
-import sys
 import numpy as np
 import pandas as pd
 import streamlit as st
 
-st.text_input("TJM numero 1", key="TJ_1",value=650)
-st.text_input("TJM numero 2", key="TJ_2",value=750)
-st.text_input("TJM numero 3", key="TJ_3",value=800)
+# st.set_page_config(layout="wide")
 
-total_sum = st.slider("Somme totale à moduler", min_value=0, max_value=100000,step=25)
+titre_str = "Assistant de Facturation Datatorii"
+
+st.set_page_config(
+    page_title=titre_str,
+    page_icon="DtrLogoV2Full_Dark.svg",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+
+
+st.image("DtrLogoV2Full_Dark.svg", width=150)
+
+st.title(titre_str)
+
+st.header("Comment tomber juste ?")
+
+st.write("Entrez les valeurs en Euros des TJMs des consultants ainsi que la somme totale à facturer.")
+
+st.text_input("TJM numero 1 :", key="TJ_1", value=650)
+st.text_input("TJM numero 2 :", key="TJ_2", value=750)
+st.text_input("TJM numero 3 :", key="TJ_3", value=800)
+
+total_sum = st.slider("Somme totale à moduler", min_value=0, max_value=100000, step=25)
 
 TJ_1 = int(st.session_state.TJ_1)
 TJ_2 = int(st.session_state.TJ_2)
 TJ_3 = int(st.session_state.TJ_3)
 
-# incremental integer 
+# incremental integer
 increment = 0.25
 
-TJ_list = [TJ_1,TJ_2,TJ_3]
-max_day_sum = 2*total_sum/(max(TJ_list))
+TJ_list = [TJ_1, TJ_2, TJ_3]
+max_day_sum = 2 * total_sum / (max(TJ_list))
 
-range_vector = np.arange(0,max_day_sum,increment)
+range_vector = np.arange(0, max_day_sum, increment)
 
-mesh_array = np.array(np.meshgrid(range_vector,range_vector,range_vector)).T.reshape(-1,3)
+mesh_array = np.array(np.meshgrid(range_vector, range_vector, range_vector)).T.reshape(-1, 3)
 
-df_solutions = pd.DataFrame(mesh_array,columns=[f"TJ1 - {TJ_1} €",f"TJ2 - {TJ_2} €",f"TJ3 - {TJ_3} €"])
-df_solutions["Total"]=df_solutions.iloc[:, 0]*TJ_1 + df_solutions.iloc[:, 1]*TJ_2 + df_solutions.iloc[:, 2]*TJ_3
+df_solutions = pd.DataFrame(mesh_array, columns=[f"TJ1 - {TJ_1} €", f"TJ2 - {TJ_2} €", f"TJ3 - {TJ_3} €"])
+df_solutions["Total"] = df_solutions.iloc[:, 0] * TJ_1 + df_solutions.iloc[:, 1] * TJ_2 + df_solutions.iloc[:, 2] * TJ_3
 
-st.dataframe(df_solutions[df_solutions["Total"]==total_sum])
+st.write(
+    f"Voici les combinaisons possibles. Les colonnes peuvent êtres triées pour minimiser ou maximiser"
+    f" certaines prestations. L'incrément est fixé à {increment} jour."
+)
+
+st.dataframe(df_solutions[df_solutions["Total"] == total_sum], hide_index=True, use_container_width=True)
